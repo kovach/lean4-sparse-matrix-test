9b831ffbf5087e16062eb3ad6a251db783bcf160
Scott Kovach <dskovach@gmail.com>
2021-02-22 12:49:43 -0800

    add HasZero/HasOne

---
+++ Tensor/Has.lean
@@ -0,0 +1,6 @@
+class HasZero (α : Type _) where zero : α
+class HasOne (α : Type _) where one : α
+instance : HasZero Nat where zero := 0
+instance : HasOne  Nat where one := 1
+instance : HasZero Float where zero := 0.0
+instance : HasOne  Float where one := 1.0
